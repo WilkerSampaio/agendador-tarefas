0c180598fb2ea6f6234465c105e97a7602f5bc13
Wilker <neoxextreme@gmail.com>
2025-08-24 13:57:40 -0400

    tratando exceções

---
+++ src/main/java/com/wilker/agendadortarefas/infrastructure/exception/UnauthorizedException.java
@@ -0,0 +1,9 @@
+package com.wilker.agendadortarefas.infrastructure.exception;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class UnauthorizedException extends AuthenticationException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
